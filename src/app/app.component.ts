8b5c97152ab3e404487ff9dd0da7e23ab326ecc3
AnishMandal939 <mandalktaanees12@gmail.com>
2024-03-14 09:01:31 +0545

    feat - Add CSS styles and update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ecommerce';
+
+  cartProducts: any[] = [];
+  subTotal: number = 0;
+  constructor(private productService: ProductService, private router: Router){
+    this.productService.cartAddedSubject.subscribe((res: any) => {
+      this.loadCart();
+    })
+  }
+  ngOnInit(): void {
+    this.loadCart();
+  }
+  redirectToSale(){
+    this.router.navigateByUrl('sale')
+  }
+  loadCart(){
+    this.subTotal = 0;
+    this.productService.getCartItemsByCustomerId(1).subscribe((res: any) => {
+      this.cartProducts = res.data;
+      // debugger;
+      this.cartProducts.forEach((subTotalItem: any) => {
+        this.subTotal = this.subTotal + subTotalItem.productPrice;
+      })
+    })
+  }
 }
